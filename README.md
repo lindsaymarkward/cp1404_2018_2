f815f628a0ac1f1c341e8a5cecf68e559a36bebb
Lindsay Ward <lindsay.ward@jcu.edu.au>
2018-07-25 16:27:01 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CP1404 2018 SP2 In-class Demos
+
+This repository contains the in-class code from CP1404 in 2018 SP2 at JCU in Townsville - from Lindsay Ward.
+
+_It is not intended to be best-practice, well-documented code._
+Some of it will be just fine, but I usually make no extra effort after class to "fix up" or complete examples.
---
+++ week_01/hello.py
@@ -0,0 +1,26 @@
+import math
+from random import randint
+
+print("Hello, world")
+
+# print(math.pi)
+# x = randint(1, 10)
+# print(x)
+# x += 1
+# print(x)
+
+# number = int(input("Number: "))
+# while number > 0:
+#     print(number * number)
+#     print()
+#     # print("Thanks")
+#     number = int(input("Number: "))
+#
+# print("Done")
+
+# for character in "Hello world":
+#     print(character)
+
+for i in range(0, 10000, 100):
+    print(i, end=" ")
+
