822f592096ee439138904d2c5b86ba33fadf0612
Lindsay Ward <lindsay.ward@jcu.edu.au>
2018-08-22 15:43:15 +1000

    Update warmup with comments.

--- week_05/dictionaries.py
+++ week_05/dictionaries.py
@@ -2,14 +2,14 @@
 
 
 def main():
-    # names = ["Bill", "Jane", "Sven"]
-    # ages = [21, 34, 56]
-    names = []
-    ages = []
+    """Main program."""
+    names = ["Bill", "Jane", "Sven"]
+    ages = [21, 34, 56]
     print(find_oldest(names, ages))
 
 
 def find_oldest(names, ages):
+    """Find oldest in names/ages parallel lists."""
     highest_age = -1
     highest_age_index = -1
     for i, age in enumerate(ages):
